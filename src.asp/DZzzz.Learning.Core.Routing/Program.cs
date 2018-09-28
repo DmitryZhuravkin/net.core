a3064fb856e82378f56899f93db03614a8fbc3fe
Dmitry.Zhuravkin <demion_07@list.ru>
2018-09-28 17:08:08 +0300

    Routing base solution + Semantic UI

---
+++ src.asp/DZzzz.Learning.Core.Routing/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Hosting;
+
+// libman.json - define client0side dependencies
+
+namespace DZzzz.Learning.Core.Routing
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            return new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>();
+        }
+    }
+}
