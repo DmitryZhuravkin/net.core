1c3e3086619d0d7b67f24e25a032c1b2f452ee48
Dmitry.Zhuravkin <demion_07@list.ru>
2018-09-28 15:19:50 +0300

    App config project

---
+++ src.asp/DZzzz.Learning.Core.App.Configuration/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+
+namespace DZzzz.Learning.Core.App.Configuration
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            return new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .ConfigureAppConfiguration((hostingContext, config) => // method to configure application
+                {
+                    config.AddJsonFile("appsettings.json", true, true); // load from external file
+                    config.AddEnvironmentVariables();
+
+                    if (args != null)
+                    {
+                        config.AddCommandLine(args);
+                    }
+                })
+                .UseIISIntegration()
+                .UseStartup<Startup>();
+        }
+    }
+}
---
+++ src.asp/DZzzz.Learning.Core.App.Configuration/Startup.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DZzzz.Learning.Core.App.Configuration
+{
+    public class Startup
+    {
+        private readonly IConfiguration configuration;
+
+        public Startup(IConfiguration configuration) // NOTE: we can pass IConfiguration object to startup class
+        {
+            this.configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Run(async context =>
+            {
+                bool boolValue = configuration.GetSection("CustomTestConfiguration").GetValue<bool>("Option1");
+                int intValue = configuration.GetSection("CustomTestConfiguration").GetValue<int>("Option2");
+                string stringValue = configuration.GetSection("CustomTestConfiguration").GetValue<string>("Option3");
+
+                await context.Response.WriteAsync($"Hello World! App config: {boolValue}, {intValue}, {stringValue}");
+            });
+        }
+    }
+}
