d40bff08b688f42a99c55745379c718ad1301757
Dmitry.Zhuravkin <demion_07@list.ru>
2018-10-22 18:01:15 +0300

    Form tag helpers

---
+++ src.asp/DZzzz.Learning.Core.Form.Tag.Helpers/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using DZzzz.Learning.Core.Form.Tag.Helpers.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DZzzz.Learning.Core.Form.Tag.Helpers.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IRepository repository;
+
+        public HomeController(IRepository repo)
+        {
+            repository = repo;
+        }
+
+        public ViewResult Index() => View(repository.Cities);
+
+        public ViewResult Create() => View();
+
+        public ViewResult Edit() => View("Create", repository.Cities.FirstOrDefault());
+
+        [HttpPost]
+        [ValidateAntiForgeryToken] // don`t forget to do this for ALL port request
+        public IActionResult Create(City city)
+        {
+            repository.AddCity(city);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ src.asp/DZzzz.Learning.Core.Form.Tag.Helpers/Models/City.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DZzzz.Learning.Core.Form.Tag.Helpers.Models
+{
+    public class City
+    {
+        //[UIHint()] - attribute is used to override default mapping between c# type and browser type attribute
+        public string Name { get; set; }
+        public string Country { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:F2}", ApplyFormatInEditMode = true)] // data format
+        public int? Population { get; set; }
+    }
+}
