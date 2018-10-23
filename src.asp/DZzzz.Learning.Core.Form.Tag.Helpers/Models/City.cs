f468df1216e8b4f867843b156ee1531e01e77fb5
Dmitry.Zhuravkin <demion_07@list.ru>
2018-10-23 11:31:31 +0300

    Form tag helpers

--- src.asp/DZzzz.Learning.Core.Form.Tag.Helpers/Models/City.cs
+++ src.asp/DZzzz.Learning.Core.Form.Tag.Helpers/Models/City.cs
@@ -5,10 +5,14 @@ namespace DZzzz.Learning.Core.Form.Tag.Helpers.Models
     public class City
     {
         //[UIHint()] - attribute is used to override default mapping between c# type and browser type attribute
+        [Display(Name = "City")] // override asp-for tag helper value for label
         public string Name { get; set; }
+
         public string Country { get; set; }
 
         [DisplayFormat(DataFormatString = "{0:F2}", ApplyFormatInEditMode = true)] // data format
         public int? Population { get; set; }
+
+        public string Notes { get; set; }
     }
 }
---
+++ src.asp/DZzzz.Learning.Core.Form.Tag.Helpers/Models/CountryNames.cs
@@ -0,0 +1,10 @@
+﻿namespace DZzzz.Learning.Core.Form.Tag.Helpers.Models
+{
+    public enum CountryNames
+    {
+        UK,
+        USA,
+        France,
+        China
+    }
+}
