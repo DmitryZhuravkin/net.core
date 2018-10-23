becb09ebd6fc043563cabe5f563784a18044b71c
Dmitry.Zhuravkin <demion_07@list.ru>
2018-10-23 15:34:45 +0300

    Built-in tag helpers

---
+++ src.asp/DZzzz.Learning.Core.BuiltIn.Tag.Helpers/Components/TimeViewComponent.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DZzzz.Learning.Core.BuiltIn.Tag.Helpers.Components
+{
+    public class TimeViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke() => View(DateTime.Now);
+    }
+}
