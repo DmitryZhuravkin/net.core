0bab15b6434b1d55a7b91af5ffc36e9261ea08d2
Dmitry.Zhuravkin <demion_07@list.ru>
2018-10-25 15:31:25 +0300

    Identity init

---
+++ src.asp/DZzzz.Learning.Core.Identity.Init/Startup.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DZzzz.Learning.Core.Identity.Init
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseStatusCodePages();
+            app.UseDeveloperExceptionPage();
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
